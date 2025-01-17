54ef1951b93edbcc3cb1e4665b6b94e800a3341a
Giovanni <giovanni_ge@hotmail.com>
2025-01-17 16:48:17 -0300

    feat: adicionando favoritos

--- projeto-intercode/src/Pages/Favoritos/index.jsx
+++ projeto-intercode/src/Pages/Favoritos/index.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import Banner from '../../Components/Banner'
+import Titulo from '../../Components/Titulo'
+import styles from "./Favoritos.module.css";
+import Card from '../../Components/Card';
 
 export default function Favoritos() {
   return (
-    <div>Favoritos</div>
+    <>
+      <Banner imagem="favoritos" />
+      <Titulo>
+        <h1>Meus Favoritos</h1>
+      </Titulo>
+      <section className={styles.container}>
+        <Card id="1" titulo="Gato bonifácio" capa="https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg" />
+      </section>
+    </>
   )
 }
