987f2e6100440c134cd5744ab88c858d5e3f39ab
giovanni <giovanni_ge@hotmai.com>
2025-01-16 22:26:47 -0300

    feat: organizando titulo

--- projeto-intercode/src/Pages/Inicio/index.jsx
+++ projeto-intercode/src/Pages/Inicio/index.jsx
@@ -1,12 +1,16 @@
 import Rodape from "../../Components/Rodape";
 import Cabecalho from "../../Components/Cabecalho";
 import Banner from "../../Components/Banner";
+import Titulo from "../../Components/Titulo";
 
 export default function Inicio() {
   return (
     <>
       <Cabecalho />
       <Banner imagem="home" />
+      <Titulo>
+        <h1>Um lugar para guardar seu vídeos e filmes!</h1>
+      </Titulo>
       <Rodape />
     </>
   )
