c3ab4a4b99848979a4477586808dc5b7e66ef082
Giovanni <giovanni_ge@hotmail.com>
2025-01-18 15:47:03 -0300

    feat: fazendo o icon de favoritar com contexto

--- projeto-intercode/src/Components/Card/index.jsx
+++ projeto-intercode/src/Components/Card/index.jsx
@@ -1,15 +1,25 @@
 import styles from "./Card.module.css";
 import iconeFavoritar from './favoritar.png';
+import iconeDesvaloritar from './desfavoritar.png';
+
+import { useFavoritoContext } from "../../Contextos/Favoritos";
 
 export default function Card({ id, titulo, capa }) {
+  const { favorito, adicionarFavorito } = useFavoritoContext();
+  const ehFavorito = favorito.some((fav) => fav.id === id);
+  const icon = !ehFavorito ? iconeFavoritar : iconeDesvaloritar;
+
   return (
     <div className={styles.container}>
       <img src={capa} alt={titulo} className={styles.capa} />
       <h2>{titulo}</h2>
       <img 
-        src={iconeFavoritar} 
+        src={icon} 
         alt="Favoritar Filme" 
         className={styles.favoritar}
+        onClick={() => {
+          adicionarFavorito({id, titulo, capa })
+        }}
     />
     </div>
   );
